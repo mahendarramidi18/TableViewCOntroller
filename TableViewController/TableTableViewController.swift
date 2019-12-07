2aecb5e7bfcd4f4e26e037dbd1ba80cb9a2a2fe5
mahendarramidi <mahendardon18@gmail.com>
2019-12-07 13:10:07 -0500

    code edit

--- TableViewController/TableTableViewController.swift
+++ TableViewController/TableTableViewController.swift
@@ -11,33 +11,25 @@ import UIKit
 class TableTableViewController: UITableViewController {
     
     var titleArray = ["Game of Trones", "Criminal Minds", "Suites", "Narcos", "Black List"]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-       
         return 1
     }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       
         return titleArray.count
     }
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "titles", for: indexPath)
         cell.textLabel?.text = titleArray[indexPath.row]
         return cell
     }
-    
     //MARK: - Table view delegate
-    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
        // print(titleArray[indexPath.row])
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
@@ -45,10 +37,8 @@ class TableTableViewController: UITableViewController {
         }else{
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
-        
         tableView.deselectRow(at: indexPath, animated: true)
     }
-    
     //MARK: Creating the action for BarButtonItem
     @IBAction func barButtonPressed(_ sender: UIBarButtonItem) {
         var textField = UITextField()
